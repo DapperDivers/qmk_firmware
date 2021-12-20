374556106f3378ec1f6c93338d3e0a195ae1456b
Derek Mackley <Derek.mackley@hotmail.com>
2021-12-20 18:17:16 -0500

    removes rgb and modifies keymap

--- keyboards/crkbd/keymaps/barediver/keymap.c
+++ keyboards/crkbd/keymaps/barediver/keymap.c
@@ -24,13 +24,13 @@ char layer_state_str[24];
 
   enum userspace_layers {
     _DEFAULTS = 0,
-	_COLEMAK = 0,
-	_COLEMAKDH,
-    _QWERTY,
+	_COLEMAKDH = 0,
     _NUM,
     _SYM,
     _COMMAND,
     _NUMPAD,
+    _COLEMAK,
+    _QWERTY,
 	_SWITCH,
     _MOVE,
 
@@ -38,45 +38,18 @@ char layer_state_str[24];
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-		// colemak
-	[_COLEMAK] = LAYOUT(
-	//,-----------------------------------------------------.                    ,-----------------------------------------------------.
-	    LT(_NUMPAD,KC_TAB), 	KC_Q, 	 KC_W, 	  KC_F,    KC_P,    KC_G, 		 LT(_SWITCH,KC_J),    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSPC,
-	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-	    KC_LSFT,    KC_A,    KC_R,    KC_S,    KC_T,    KC_D, 						  KC_H,    KC_N,    KC_E,    KC_I,LT(_NUMPAD,KC_O),KC_QUOT,
-	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-		KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, 						  KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-	//|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-										 KC_LGUI,   MO(_NUM),  KC_SPC, 	KC_ENT,    MO(_SYM), KC_LALT
-										//`--------------------------'  `--------------------------'
-	),
-
 			// colemak DH
 	[_COLEMAKDH] = LAYOUT(
 	//,-----------------------------------------------------.                    ,-----------------------------------------------------.
 	    LT(_NUMPAD,KC_TAB), 	KC_Q, 	 KC_W, 	  KC_F,    KC_P,    KC_B, 					LT(_SWITCH,KC_J),    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSPC,
 	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-	    KC_LSFT,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G, 						  KC_M,    KC_N,    KC_E,    KC_I,LT(_NUMPAD,KC_O),KC_QUOT,
+	    KC_LSFT,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G, 						  KC_M,    KC_N,    KC_E,    KC_I, KC_O, KC_QUOT,
 	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-		KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V, 						  KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+		KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V, 						  KC_K,    KC_H, KC_DOT, KC_COMM, KC_SLSH, KC_RSFT,
 	//|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-										 KC_LGUI,   MO(_NUM),  KC_SPC, 	KC_ENT,    MO(_SYM), KC_LALT
+										 KC_LGUI,   MO(_NUM),  KC_ENT, 	KC_SPC,    MO(_SYM), KC_LALT
 										//`--------------------------'  `--------------------------'
 	),
-
-	// qwerty
-	[_QWERTY] = LAYOUT(
-	//,-----------------------------------------------------.                    ,-----------------------------------------------------.
-	KC_TRNS, 	KC_Q, 	KC_W, 	KC_E, 	KC_R, 	KC_T, 							LT(_SWITCH,KC_Y), KC_U, 	KC_I, 	 KC_O, 	 KC_P, 	KC_TRNS,
-	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-	KC_TRNS, 		KC_A, 	KC_S, 	KC_D, 	KC_F, 	KC_G, 							KC_H, 	  KC_J, 	KC_K, 	 KC_L, LT(_NUMPAD,KC_SCLN), KC_TRNS,
-	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-	KC_TRNS, 		KC_Z, 	KC_X, 	KC_C, 	KC_V, 	KC_B, 							KC_N, 	  KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_TRNS,
-    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-										 KC_TRNS,   KC_TRNS,  KC_TRNS, 	KC_TRNS,    KC_TRNS, KC_TRNS
-									   //`--------------------------'  `--------------------------'
-	),
-
 
 
 	// numbers  - L thumb
@@ -84,8 +57,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//,-----------------------------------------------------.                    ,-----------------------------------------------------.
 	     KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, 					      KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
 	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-	    KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_WH_U,   KC_PGUP, 				   KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,   KC_NO,  KC_DEL,
-	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+	    KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_WH_U,   KC_PGUP, 				   KC_NO, KC_UP,   KC_DOWN, KC_LEFT,   KC_RGHT,  KC_DEL,
+	//|--------+--------+--------+-------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
 		KC_TRNS,   KC_NO,   KC_NO,   KC_NO,   KC_WH_D,   KC_PGDN,					     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
 	//|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
 											KC_TRNS, KC_TRNS,  KC_TRNS,     KC_TRNS,   MO(_COMMAND), KC_TRNS
@@ -110,12 +83,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//,-----------------------------------------------------.                    ,-----------------------------------------------------.
 	      RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, 						 KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_NO,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-	    RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,   DF(1),   DF(0), 					 C(G(KC_LEFT)),   KC_NO,   KC_NO,   C(G(KC_RGHT)),   KC_NO,   KC_NO,
+	    RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,   DF(1),   DF(0), 					 KC_NO,   C(A(KC_UP)),   KC_NO,   C(A(KC_LEFT)),   C(A(KC_RGHT)),   KC_NO,
 	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
 		RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,   KC_NO,   DF(2), 					     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
 	//|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
 										    KC_TRNS, KC_TRNS,  KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
 										//`--------------------------'  `--------------------------'
+	),
+		// colemak
+	[_COLEMAK] = LAYOUT(
+	//,-----------------------------------------------------.                    ,-----------------------------------------------------.
+	    LT(_NUMPAD,KC_TAB), 	KC_Q, 	 KC_W, 	  KC_F,    KC_P,    KC_G, 		 LT(_SWITCH,KC_J),    KC_L,    KC_U,    KC_Y, KC_SCLN, KC_BSPC,
+	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+	    KC_LSFT,    KC_A,    KC_R,    KC_S,    KC_T,    KC_D, 						  KC_H,    KC_N,    KC_E,    KC_I,LT(_NUMPAD,KC_O),KC_QUOT,
+	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+		KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, 						  KC_K,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+	//|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+										 KC_LGUI,   MO(_NUM),  KC_SPC, 	KC_ENT,    MO(_SYM), KC_LALT
+										//`--------------------------'  `--------------------------'
+	),
+
+	// qwerty
+	[_QWERTY] = LAYOUT(
+	//,-----------------------------------------------------.                    ,-----------------------------------------------------.
+	KC_TRNS, 	KC_Q, 	KC_W, 	KC_E, 	KC_R, 	KC_T, 							LT(_SWITCH,KC_Y), KC_U, 	KC_I, 	 KC_O, 	 KC_P, 	KC_TRNS,
+	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+	KC_TRNS, 		KC_A, 	KC_S, 	KC_D, 	KC_F, 	KC_G, 							KC_H, 	  KC_J, 	KC_K, 	 KC_L, LT(_NUMPAD,KC_SCLN), KC_TRNS,
+	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+	KC_TRNS, 		KC_Z, 	KC_X, 	KC_C, 	KC_V, 	KC_B, 							KC_N, 	  KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_TRNS,
+    //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+										 KC_TRNS,   KC_TRNS,  KC_TRNS, 	KC_TRNS,    KC_TRNS, KC_TRNS
+									   //`--------------------------'  `--------------------------'
 	),
 
 	// numpad
@@ -164,97 +162,97 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // it appears that these are different to the board numbering.
 // when you specify n here, it lightss up n+1 on the board diagram - actually may be an entirely different pattern
 
-// _QWERTY,
-// Light on inner column and underglow
-const rgblight_segment_t PROGMEM layer_qwerty_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 10, HSV_AZURE}
-);
+// // _QWERTY,
+// // Light on inner column and underglow
+// const rgblight_segment_t PROGMEM layer_qwerty_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+//     {0, 10, HSV_AZURE}
+// );
 
-// _COLEMAKDH,
-// Light on inner column and underglow
-const rgblight_segment_t PROGMEM layer_colemakdh_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 10, HSV_RED}
-);
+// // _COLEMAKDH,
+// // Light on inner column and underglow
+// const rgblight_segment_t PROGMEM layer_colemakdh_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+//     {0, 10, HSV_RED}
+// );
 
-// _NUM,
-// Light on inner column and underglow
-const rgblight_segment_t PROGMEM layer_num_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 10, HSV_TEAL}
-);
-// _SYMBOL,
-// Light on inner column and underglow
-const rgblight_segment_t PROGMEM layer_symbol_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 10, HSV_BLUE}
-);
-// _COMMAND,
-// Light on inner column and underglow
-const rgblight_segment_t PROGMEM layer_command_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 10, HSV_PURPLE}
-);
-
-
-//_NUMPAD
-//havent worked out how to do each side individually either
-const rgblight_segment_t PROGMEM layer_numpad_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-{0, 10, HSV_ORANGE}
-);
-const rgblight_segment_t PROGMEM layer_numpad_rh_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-	{0, 10, HSV_ORANGE},
-	{10, 5, HSV_BLUE},
-    {15, 3, HSV_BLUE},
-	{18, 3, HSV_BLUE}
-);
-
-// _MOVE,
-// Light on inner column and underglow
-const rgblight_segment_t PROGMEM layer_move_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 10, HSV_PINK}
-);
-
-// _SWITCHER   // light up top row
-const rgblight_segment_t PROGMEM layer_switcher_lights[] = RGBLIGHT_LAYER_SEGMENTS(
-    {0, 6, HSV_GREEN},
-	{9, 2, HSV_GREEN},
-	{17, 2, HSV_GREEN},
-	{23, 2, HSV_GREEN}
-);
+// // _NUM,
+// // Light on inner column and underglow
+// const rgblight_segment_t PROGMEM layer_num_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+//     {0, 10, HSV_TEAL}
+// );
+// // _SYMBOL,
+// // Light on inner column and underglow
+// const rgblight_segment_t PROGMEM layer_symbol_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+//     {0, 10, HSV_BLUE}
+// );
+// // _COMMAND,
+// // Light on inner column and underglow
+// const rgblight_segment_t PROGMEM layer_command_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+//     {0, 10, HSV_PURPLE}
+// );
 
 
-// Now define the array of layers. Later layers take precedence
-const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
-    layer_qwerty_lights,
-	layer_colemakdh_lights,
-	layer_num_lights,// overrides layer 1
-	layer_symbol_lights,
-    layer_command_lights,
-	layer_numpad_lights,
-	layer_numpad_rh_lights,
-	layer_move_lights,
-	layer_switcher_lights  // Overrides other layers
-);
+// //_NUMPAD
+// //havent worked out how to do each side individually either
+// const rgblight_segment_t PROGMEM layer_numpad_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+// {0, 10, HSV_ORANGE}
+// );
+// const rgblight_segment_t PROGMEM layer_numpad_rh_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+// 	{0, 10, HSV_ORANGE},
+// 	{10, 5, HSV_BLUE},
+//     {15, 3, HSV_BLUE},
+// 	{18, 3, HSV_BLUE}
+// );
+
+// // _MOVE,
+// // Light on inner column and underglow
+// const rgblight_segment_t PROGMEM layer_move_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+//     {0, 10, HSV_PINK}
+// );
+
+// // _SWITCHER   // light up top row
+// const rgblight_segment_t PROGMEM layer_switcher_lights[] = RGBLIGHT_LAYER_SEGMENTS(
+//     {0, 6, HSV_GREEN},
+// 	{9, 2, HSV_GREEN},
+// 	{17, 2, HSV_GREEN},
+// 	{23, 2, HSV_GREEN}
+// );
+
+
+// // Now define the array of layers. Later layers take precedence
+// const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+//     layer_qwerty_lights,
+// 	layer_colemakdh_lights,
+// 	layer_num_lights,// overrides layer 1
+// 	layer_symbol_lights,
+//     layer_command_lights,
+// 	layer_numpad_lights,
+// 	layer_numpad_rh_lights,
+// 	layer_move_lights,
+// 	layer_switcher_lights  // Overrides other layers
+// );
 
 void keyboard_post_init_user(void) {
     // Enable the LED layers
-    rgblight_layers = my_rgb_layers;
-	rgblight_mode(10);// haven't found a way to set this in a more useful way
+    //rgblight_layers = my_rgb_layers;
+	//rgblight_mode(10);// haven't found a way to set this in a more useful way
 
 }
 
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-	rgblight_set_layer_state(0, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_QWERTY));
-	rgblight_set_layer_state(1, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_QWERTY));
+// layer_state_t layer_state_set_user(layer_state_t state) {
+// 	rgblight_set_layer_state(0, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_QWERTY));
+// 	rgblight_set_layer_state(1, layer_state_cmp(state, _DEFAULTS) && layer_state_cmp(default_layer_state,_QWERTY));
 
-	rgblight_set_layer_state(2, layer_state_cmp(state, _NUM));
-	rgblight_set_layer_state(3, layer_state_cmp(state, _SYM));
-	rgblight_set_layer_state(4, layer_state_cmp(state, _COMMAND));
-	rgblight_set_layer_state(5, layer_state_cmp(state, _NUMPAD));
-	if (!is_keyboard_master())
-		rgblight_set_layer_state(6, layer_state_cmp(state, _NUMPAD));
-	rgblight_set_layer_state(7, layer_state_cmp(state, _MOVE));
-	rgblight_set_layer_state(8, layer_state_cmp(state, _SWITCH));
-    return state;
-}
+// 	rgblight_set_layer_state(2, layer_state_cmp(state, _NUM));
+// 	rgblight_set_layer_state(3, layer_state_cmp(state, _SYM));
+// 	rgblight_set_layer_state(4, layer_state_cmp(state, _COMMAND));
+// 	rgblight_set_layer_state(5, layer_state_cmp(state, _NUMPAD));
+// 	if (!is_keyboard_master())
+// 		rgblight_set_layer_state(6, layer_state_cmp(state, _NUMPAD));
+// 	rgblight_set_layer_state(7, layer_state_cmp(state, _MOVE));
+// 	rgblight_set_layer_state(8, layer_state_cmp(state, _SWITCH));
+//     return state;
+// }
 
 /*
 bool led_update_user(led_t led_state) {
--- keyboards/crkbd/keymaps/barediver/rules.mk
+++ keyboards/crkbd/keymaps/barediver/rules.mk
@@ -1,5 +1,12 @@
 MOUSEKEY_ENABLE = yes
 EXTRAKEY_ENABLE = yes
-RGBLIGHT_ENABLE = yes
+
+RGBLIGHT_ENABLE = no
+AUDIO_ENABLE = no
+
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
+
+
+#https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
+EXTRAFLAGS += -flto
