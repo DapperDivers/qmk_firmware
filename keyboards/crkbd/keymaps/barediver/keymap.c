58176d7f91af8b77c176505240fe1ef1659fa669
Derek Mackley <Derek.mackley@hotmail.com>
2022-03-21 16:44:07 -0400

    upadate keymap

--- keyboards/crkbd/keymaps/barediver/keymap.c
+++ keyboards/crkbd/keymaps/barediver/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
 	    KC_LSFT,    KC_A,    KC_R,    KC_S,    KC_T,    KC_G, 						  KC_M,    KC_N,    KC_E,    KC_I, KC_O, KC_QUOT,
 	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-		KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V, 						  KC_K,    KC_H, KC_DOT, KC_COMM, KC_SLSH, KC_RSFT,
+		KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V, 						  KC_K,    KC_H, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
 	//|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
 										 KC_LGUI,   MO(_NUM),  KC_SPC, 	KC_ENT,    MO(_SYM), KC_LALT
 										//`--------------------------'  `--------------------------'
@@ -83,9 +83,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//,-----------------------------------------------------.                    ,-----------------------------------------------------.
 	      RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, 						 KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_DEL,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-	    RGB_TOG, RGB_HUI, RGB_SAI, KC_F11,   KC_F12,   DF(0), 					 KC_NO,   C(A(KC_UP)),   KC_NO,   C(A(KC_LEFT)),   C(A(KC_RGHT)),   KC_NO,
+	    RGB_TOG, RGB_HUI, RGB_SAI, KC_F11,   KC_F12,   DF(0), 					 KC_NO,   KC_PGUP,   KC_PGDN,   KC_HOME,  KC_END,   KC_NO,
 	//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-		RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,   DF(1),   DF(2), 					    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+		RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD,   DF(1),   DF(2), 					    KC_NO,   KC_PSCR,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
 	//|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
 										    KC_TRNS, KC_TRNS,  KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
 										//`--------------------------'  `--------------------------'
